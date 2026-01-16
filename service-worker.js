6ec3527c8fecaf933f5468a8c5f44e8bb5d802ef
Cristofer Cavalcante <cristofermaciel312@gmail.com>
2026-01-16 20:57:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,32 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("controle-gastos-v2").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "historico.html"
-      ]);
-    })
+const CACHE_NAME = 'controle-gastos-v2';
+const FILES = [
+  './',
+  './index.html',
+  './historico.html',
+  './script.js',
+  './historico.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => k !== CACHE_NAME && caches.delete(k))
+      )
+    )
   );
+  self.clients.claim();
 });
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
